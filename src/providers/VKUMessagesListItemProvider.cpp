86c9a25c9fe39efa3b62699aaf1ecc1c1c5a35cf
Igor Glotov <glotov@jelastic.com>
2013-11-14 16:53:27 +0700

    timestamps

--- src/providers/VKUMessagesListItemProvider.cpp
+++ src/providers/VKUMessagesListItemProvider.cpp
@@ -7,6 +7,7 @@
 
 #include "VKUMessagesListItemProvider.h"
 #include "JsonParseUtils.h"
+#include "TimeUtils.h"
 
 using namespace Tizen::Ui::Controls;
 using namespace Tizen::Graphics;
@@ -18,7 +19,7 @@ using namespace Tizen::Ui::Scenes;
 using namespace Tizen::Ui;
 
 static const int LIST_HEIGHT = 10000;
-
+static const int TIMESTAMP_TEXT_COLOR = 0x6d7175;
 #define USE_CACHE 1
 
 VKUMessagesListItemProvider::VKUMessagesListItemProvider() {
@@ -46,8 +47,7 @@ TableViewItem* VKUMessagesListItemProvider::CreateItem(int index, int itemWidth)
 	AppLog("Create item");
 
 	MessageBubble* pMessageBubble;
-	Panel* placeholder;
-	VerticalBoxLayout placeholderLayout;
+	RelativeLayout itemLayout;
 
 	JsonObject *itemObject;
 	IJsonValue *itemValue;
@@ -55,6 +55,10 @@ TableViewItem* VKUMessagesListItemProvider::CreateItem(int index, int itemWidth)
 	IJsonValue *outValue;
 	TableViewItem* pItem;
 	JsonNumber outNumber;
+
+	Label *pTimeStamp;
+	String timespampText;
+	int timestampValue;
 
 	String messageText(L"no text????");
 	int out = 0;
@@ -68,21 +72,11 @@ TableViewItem* VKUMessagesListItemProvider::CreateItem(int index, int itemWidth)
 	TryCatch(r == E_SUCCESS, , "Failed GetAt");
 	itemObject = static_cast<JsonObject *>(itemValue);
 
-	static const String bodyConst(L"body");
-	if (itemObject->GetValue(&bodyConst, bodyValue) == E_SUCCESS) {
-		if (bodyValue->GetType() == JSON_TYPE_STRING) {
-			messageText = *static_cast<JsonString *>(bodyValue);
-		}
-	}
+	JsonParseUtils::GetString(*itemObject, L"body", messageText);
+	JsonParseUtils::GetInteger(*itemObject, L"out", out);
+	JsonParseUtils::GetInteger(*itemObject, L"date", timestampValue);
 
-	static const String outConst(L"out");
-	if (itemObject->GetValue(&outConst, outValue) == E_SUCCESS) {
-		if (outValue->GetType() == JSON_TYPE_NUMBER) {
-			outNumber = *static_cast<JsonNumber *>(outValue);
-			out = outNumber.ToInt();
-		}
-	}
-
+	TimeUtils::GetDialogsTime(timestampValue, timespampText);
 
 	// create rich text panel
 	AppLog("Message is %d == out", out);
@@ -92,26 +86,43 @@ TableViewItem* VKUMessagesListItemProvider::CreateItem(int index, int itemWidth)
 	TryCatch(r == E_SUCCESS, , "Failed Construct RichTextPanel");
 	AppLog("RTPanel created and constructed");
 
-	placeholderLayout.Construct(VERTICAL_DIRECTION_UPWARD);
-	// create placeholder
-	placeholder = new Panel();
-	r = placeholder->Construct(placeholderLayout, Rectangle(0, 0, itemWidth, pMessageBubble->GetHeight()));
-	TryCatch(r == E_SUCCESS, , "Failed Construct RichTextPanel");
+//	placeholderLayout.Construct(VERTICAL_DIRECTION_UPWARD);
+	itemLayout.Construct();
 
-	// add bubble to placeholder
-	placeholder->AddControl(pMessageBubble);
-	if (out == 1) {
-		placeholderLayout.SetHorizontalAlignment(*pMessageBubble, LAYOUT_HORIZONTAL_ALIGN_RIGHT);
-	}
+	// timestamp label
+	pTimeStamp = new Label();
+	pTimeStamp->Construct(Rectangle(0, 0, 100, 28), timespampText);
+	pTimeStamp->SetTextConfig(28, LABEL_TEXT_STYLE_NORMAL);
+	pTimeStamp->SetTextColor(Color(TIMESTAMP_TEXT_COLOR, false));
 
 	// create table item
 	pItem = new TableViewItem();
-	r = pItem->Construct(Dimension(itemWidth, pMessageBubble->GetHeight() + 2*BUBBLE_VERTICAL_MARGIN));
+	r = pItem->Construct(itemLayout, Dimension(itemWidth, pMessageBubble->GetHeight() + 2*BUBBLE_VERTICAL_MARGIN));
 	TryCatch(r == E_SUCCESS, , "Failed GetAt");
 
 	// add rich text panel to table item
-	r = pItem->AddControl(placeholder);
+	r = pItem->AddControl(pMessageBubble);
 	TryCatch(r == E_SUCCESS, , "Failed AddControl");
+	r = pItem->AddControl(pTimeStamp);
+
+	itemLayout.SetCenterAligned(*pMessageBubble, CENTER_ALIGN_VERTICAL);
+	itemLayout.SetHorizontalFitPolicy(*pTimeStamp, FIT_POLICY_CONTENT);
+
+	if (out == 1) {
+		itemLayout.SetRelation(*pMessageBubble, *pItem, RECT_EDGE_RELATION_RIGHT_TO_RIGHT);
+		itemLayout.SetMargin(*pMessageBubble, 0, 10, 0, 0);
+
+		itemLayout.SetRelation(*pTimeStamp, *pMessageBubble, RECT_EDGE_RELATION_RIGHT_TO_LEFT);
+		itemLayout.SetRelation(*pTimeStamp, *pItem, RECT_EDGE_RELATION_BOTTOM_TO_BOTTOM);
+		itemLayout.SetMargin(*pTimeStamp, 0, 10, 0, 30);
+	} else {
+		itemLayout.SetRelation(*pMessageBubble, *pItem, RECT_EDGE_RELATION_LEFT_TO_LEFT);
+		itemLayout.SetMargin(*pMessageBubble, 10, 0, 0, 0);
+
+		itemLayout.SetRelation(*pTimeStamp, *pMessageBubble, RECT_EDGE_RELATION_LEFT_TO_RIGHT);
+		itemLayout.SetRelation(*pTimeStamp, *pItem, RECT_EDGE_RELATION_BOTTOM_TO_BOTTOM);
+		itemLayout.SetMargin(*pTimeStamp, 10, 0, 0, 30);
+	}
 
 	AppLog("Returning item");
 	return pItem;
