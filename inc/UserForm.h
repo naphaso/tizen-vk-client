60f795e8f2cf6f507c7a5cc9e5fb306d3662584d
Igor Glotov <glotov@jelastic.com>
2013-11-22 16:38:35 +0700

    More stable UserInfo

--- inc/UserForm.h
+++ inc/UserForm.h
@@ -12,11 +12,14 @@
 #include <FBase.h>
 #include <FWebJson.h>
 
+#include "IAPIRequestListener.h"
+
 class UserForm:
 	public Tizen::Ui::Controls::Form,
 	public Tizen::Ui::Scenes::ISceneEventListener,
 	public Tizen::Ui::Controls::IFormBackEventListener,
-	public Tizen::Ui::IActionEventListener {
+	public Tizen::Ui::IActionEventListener,
+	public IAPIRequestListener {
 public:
 	UserForm();
 	virtual ~UserForm();
@@ -24,6 +27,12 @@ public:
 
 	void OpenDialog();
 	void MakeCall();
+	void DeleteFriend();
+	void AddFriend();
+	void Leave();
+	void CheckIsFriends();
+	void EnableAction(Tizen::Web::Json::JsonObject * jsonObject);
+
 	result InitScene();
 
 	// Form
@@ -41,6 +50,10 @@ public:
 
 	// IActionEventListener
 	virtual void OnActionPerformed(const Tizen::Ui::Control& source, int actionId);
+
+	// IAPIRequestListener
+	virtual void OnResponseN(RequestId requestId, Tizen::Web::Json::JsonObject *object);
+
 private:
 	Tizen::Web::Json::JsonObject * _pUserJson;
 
--- src/ui/user/UserForm.cpp
+++ src/ui/user/UserForm.cpp
@@ -10,6 +10,8 @@
 #include "SceneRegister.h"
 #include "JsonParseUtils.h"
 #include "WebImageView.h"
+#include "Requests.h"
+#include "VKUApi.h"
 
 using namespace Tizen::Ui;
 using namespace Tizen::Ui::Controls;
@@ -21,6 +23,8 @@ using namespace Tizen::Graphics;
 
 static const int ACTION_ID_WRITE = 321;
 static const int ACTION_ID_CALL = 322;
+static const int ACTION_ID_DELETE = 666;
+static const int ACTION_ID_ACCEPT = 667;
 
 UserForm::UserForm() {
 	// TODO Auto-generated constructor stub
@@ -77,7 +81,10 @@ result UserForm::InitScene() {
 	name += nextName;
 
 	GetHeader()->SetTitleText(name);
-//	GetHeader()->
+	GetFooter()->SetItemEnabled(0, false);
+	GetFooter()->AddActionEventListener(*this);
+	GetFooter()->SetShowState(false);
+	GetFooter()->Invalidate(true);
 
 	if (hasMobile == 1) {
 		String phone;
@@ -92,7 +99,18 @@ result UserForm::InitScene() {
 
 	pCallButton->Invalidate(true);
 
+	CheckIsFriends();
+
 	return r;
+}
+
+void UserForm::CheckIsFriends() {
+	int userId;
+	JsonParseUtils::GetInteger(*_pUserJson, L"id", userId);
+
+	VKUApi::GetInstance().CreateRequest("friends.areFriends", this)
+		->Put(L"user_ids", Integer::ToString(userId))
+		->Submit(REQUEST_CHECK_FRIENDS);
 }
 
 // ISceneEventListener
@@ -115,6 +133,10 @@ void UserForm::OnSceneDeactivated(const Tizen::Ui::Scenes::SceneId& currentScene
 // IFormBackEventListener
 void UserForm::OnFormBackRequested(Tizen::Ui::Controls::Form& source) {
 	AppLog("You can't leave Omsk");
+	Leave();
+}
+
+void UserForm::Leave() {
 	SceneManager* pSceneManager = SceneManager::GetInstance();
 	pSceneManager->GoBackward(BackwardSceneTransition(SCENE_TRANSITION_ANIMATION_TYPE_RIGHT));
 }
@@ -142,7 +164,141 @@ void UserForm::OnActionPerformed(const Tizen::Ui::Control& source, int actionId)
 	case ACTION_ID_WRITE:
 		OpenDialog();
 		break;
+
+	case ACTION_ID_DELETE:
+		AppLog("ACTION_ID_DELETE");
+		DeleteFriend();
+		break;
+
+	case ACTION_ID_ACCEPT:
+		AppLog("ACTION_ID_ACCEPT");
+		AddFriend();
+		break;
 	}
+}
+
+void UserForm::AddFriend() {
+	int userId;
+	JsonParseUtils::GetInteger(*_pUserJson, L"id", userId);
+
+	VKUApi::GetInstance().CreateRequest("friends.add", this)
+		->Put(L"user_id", Integer::ToString(userId))
+		->Submit(REQUEST_ADD_FRIEND);
+}
+
+void UserForm::DeleteFriend() {
+	MessageBox messageBox;
+	String message;
+
+	String name, nextName;
+	JsonParseUtils::GetString(*_pUserJson, L"first_name", name);
+	name += L" ";
+	JsonParseUtils::GetString(*_pUserJson, L"last_name", nextName);
+	name += nextName;
+
+	int result = 0, userId;
+
+	JsonParseUtils::GetInteger(*_pUserJson, L"id", userId);
+
+	message = L"Remove " + name + " from friends?";
+	messageBox.Construct(L"Confirm", message, MSGBOX_STYLE_OKCANCEL, 10000);
+	messageBox.ShowAndWait(result);
+
+	switch (result) {
+	case 0:
+		return;
+		break;
+	case MSGBOX_RESULT_OK: {
+		VKUApi::GetInstance().CreateRequest("friends.delete", this)
+			->Put(L"user_id", Integer::ToString(userId))
+			->Submit(REQUEST_DELETE_FRIEND);
+	}
+	break;
+	}
+}
+
+void UserForm::OnResponseN(RequestId requestId, Tizen::Web::Json::JsonObject *object) {
+	if (requestId == REQUEST_DELETE_FRIEND) {
+		AppLog("REQUEST_DELETE_FRIEND");
+		CheckIsFriends();
+	} else if (requestId == REQUEST_CHECK_FRIENDS) {
+		AppLog("REQUEST_CHECK_FRIENDS");
+		EnableAction(object);
+	} else if (requestId == REQUEST_ADD_FRIEND) {
+		AppLog("REQUEST_CHECK_FRIENDS");
+		CheckIsFriends();
+	}
+}
+
+void UserForm::EnableAction(Tizen::Web::Json::JsonObject * jsonObject) {
+	result r = E_SUCCESS;
+	JsonArray * response;
+	JsonObject * status;
+	String name;
+	int friendStatus;
+	String actionStr = L"Remove from friends";
+
+	r = JsonParseUtils::GetArray(jsonObject, L"response", response);
+	TryCatch(r == E_SUCCESS, , "Failed JsonParseUtils::GetArray");
+
+	r = JsonParseUtils::GetObject(response, 0, status);
+	TryCatch(r == E_SUCCESS, , "Failed JsonParseUtils::GetObject");
+
+	r = JsonParseUtils::GetInteger(*status, L"friend_status", friendStatus);
+	TryCatch(r == E_SUCCESS, , "Failed JsonParseUtils::GetObject");
+
+	r = JsonParseUtils::GetString(*_pUserJson, L"first_name", name);
+
+	GetFooter()->RemoveAllItems();
+
+	switch (friendStatus) {
+	case 1: {
+
+		GetFooter()->SetShowState(false);
+	} break;
+	case 0: {
+		FooterItem item1;
+		item1.Construct(ACTION_ID_ACCEPT);
+		item1.SetText(L"Add Friend");
+		GetFooter()->AddItem(item1);
+
+		GetFooter()->SetShowState(true);
+	} break;
+	case 2:
+	{
+
+		String str = L"Add " + name + L" back";
+		FooterItem item1;
+		item1.Construct(ACTION_ID_ACCEPT);
+		item1.SetText(str);
+		GetFooter()->AddItem(item1);
+
+//		FooterItem item2;
+//		item2.Construct(ACTION_ID_DELETE);
+//		item2.SetText(L"Decline request");
+//		GetFooter()->AddItem(item2);
+
+		GetFooter()->SetShowState(true);
+	}	break;
+
+	case 3: {
+		FooterItem item2;
+		item2.Construct(ACTION_ID_DELETE);
+		item2.SetText( L"Remove from friends");
+		GetFooter()->AddItem(item2);
+
+		GetFooter()->SetShowState(true);
+	}	break;
+	}
+
+
+	GetFooter()->Invalidate(true);
+
+	return;
+CATCH:
+	AppLogException("EnableAction is failed. %s", GetErrorMessage(r));
+	SetLastResult(r);
+	return;
 }
 
 void UserForm::OpenDialog() {
